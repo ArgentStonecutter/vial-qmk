af2b8a226c8e4609cd8481de169161acd0a8adca
Argent Stonecutter <secret.argent@gmail.com>
2026-02-26 07:08:59 -0600

    Update vial keymap.c, missed previous commit.

--- keyboards/inland/mk47/keymaps/vial/keymap.c
+++ keyboards/inland/mk47/keymaps/vial/keymap.c
@@ -16,64 +16,24 @@
 
 #include QMK_KEYBOARD_H
 
-// Comment KEYMAP_DEFAULT out to get a keymap that makes more sense in terms of the actual keys:
+// This keymap mostly matches the keycaps provided by Inland, and also includes all the missing characters.
+//
+// L0 Bottom row is CTRL, CODE, ALT, HYPER(L5), LOWER(FN1/3), SPACE, RAISE(FN2/3), FN(L4), LEFT, DOWN, RIGHT
 
-// Bottom row: CTRL, CODE, ALT, HYPER(L5), LOWER(FN1/3), SPACE, RAISE(FN2/3), FN(L4), LEFT, DOWN, RIGHT
 // L1 L2 and L3 are blank for user hacking with the raise/lower keys
+
 // L4 is the function layer and completes all the standard keys:
 //     ZXCV -> -='` and ,./ -> []\ and the arrow keys map to PgUp, PgDn, Home, End
+
 // L5 is the media/backlight layer and matches the media keys on the keycaps
 //     ZXCV -> SAT-+ and HUE-+ and Enter -> RGB TOG and HYPER-SPACE is CF_TOGGLE
-
-#define KEYMAP_DEFAULT
-// Fn-space is CF_TOGGLE
+//     Plus HYPER-RAISE and HYPER_LOWER are vol-up and vol-down
 
 enum cf_keycode {
     CF_TOGGLE = QK_KB_0,
 };
 
 // clang-format off
-#ifdef KEYMAP_DEFAULT
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-    [0] = LAYOUT(
-        KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
-        KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_UP, KC_SLSH,
-        KC_LCTL, TL_UPPR, KC_LALT, KC_DEL, KC_PGDN, KC_SPC, KC_PGUP, TL_LOWR, KC_LEFT, KC_DOWN, KC_RGHT),
-
-    [1] = LAYOUT(
-        KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, RGB_MOD,
-        RGB_HUI, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MSEL, KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT, RGB_VAI, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, CF_TOGGLE, KC_TRNS, KC_TRNS, RGB_SPD, RGB_VAD, RGB_SPI),
-
-    [2] = LAYOUT(
-        KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
-        KC_CAPS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_UP, KC_SLSH,
-        KC_LCTL, KC_NO, KC_LALT, KC_DEL, KC_PGDN, KC_SPC, KC_PGUP, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT),
-
-    [3] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, CF_TOGGLE,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
-
-    [4] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
-
-    [5] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______)
-};
-#else
-// This keymap actually matches the bottom row in the default keymaps
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT(
@@ -118,7 +78,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, KC_VOLD, CF_TOGGLE,        KC_VOLU, _______, RGB_SPD, RGB_VAD, RGB_SPI)
 
 };
-#endif
 // clang-format on
 
 #ifdef RGB_MATRIX_ENABLE
